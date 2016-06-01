2a3f9b2aa090ee793cd1704ab36314e55acb9f80
颜国华 <turf111@163.com>
2016-06-01 14:16:49 +0800

    add swift code
    add a swift code for study

---
+++ swiftstudy/swiftstudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftstudy
+//
+//  Created by 颜国华 on 16/6/1.
+//  Copyright © 2016年 ooxx.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
